c7ecec1e93e157483107d3d4481cd48291d11f6b
suzana <antolin.suzana@gmail.com>
2021-10-04 14:22:40 +0200

    Add project files.

---
+++ Adapter/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Laptop laptop = new Laptop("Legion 5");
+            StandardEuSocket fTypeSocket = new StandardEuSocket();
+            Charger.Charge(laptop, fTypeSocket);
+        }
+    }
+    public interface IEuSocket
+    {
+        string giveEuCurrent();
+    }
+    public class StandardEuSocket : IEuSocket
+    {
+        public string giveEuCurrent()
+        {
+            return "current from EU socket";
+        }
+    }
+    public class Laptop
+    {
+        private string Name;
+        public Laptop(string name)
+        {
+            this.Name = name;
+        }
+        public string getName()
+        {
+            return this.Name;
+        }
+    }
+    public static class Charger
+    {
+        public static void Charge(Laptop laptop, IEuSocket socket)
+        {
+            Console.WriteLine($"Charging laptop {laptop.getName()} with {socket.giveEuCurrent()}.");
+        }
+    }
+
+}
